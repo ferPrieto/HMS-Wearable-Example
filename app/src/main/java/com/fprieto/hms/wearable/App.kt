e1b93c2da2da2fca67427de6bf9d42d065a12c5a
Fernando Prieto Moyano <fernandoprietomoyano@Fernandos-MacBook-Pro.local>
2021-06-17 17:29:36 +0100

    Added DaggerApplication

--- app/src/main/java/com/fprieto/hms/wearable/App.kt
+++ app/src/main/java/com/fprieto/hms/wearable/App.kt
@@ -1,13 +1,18 @@
 package com.fprieto.hms.wearable
 
-import android.app.Application
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
 import timber.log.Timber
 
-class App : Application() {
+class App : DaggerApplication() {
 
     override fun onCreate() {
         super.onCreate()
 
         Timber.plant(Timber.DebugTree())
     }
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
+        DaggerAppComponent.builder()
+            .build()
 }
